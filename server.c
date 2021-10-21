52d51a459f09e0c667506ac7bc321aa737ab73cf
mariadraghici <74197157+mariadraghici@users.noreply.github.com>
2021-10-21 11:56:08 +0300

    Create server.c

---
+++ server.c
@@ -0,0 +1,33 @@
+/* Copyright 2021 <Draghici Maria> */
+#include <stdlib.h>
+#include <string.h>
+
+#include "server.h"
+
+#define H_MAX 10
+
+server_memory* init_server_memory() {
+	server_memory *server = calloc(1, sizeof(server_memory));
+
+	server->ht = ht_create(H_MAX, hash_function_key, compare_function_strings);
+
+	return server;
+}
+
+void server_store(server_memory* server, char* key, char* value) {
+	ht_put(server->ht, key, strlen(key) + 1, value, strlen(value) + 1);
+}
+
+void server_remove(server_memory* server, char* key) {
+		ht_remove_entry(server->ht, key);
+}
+
+char* server_retrieve(server_memory* server, char* key) {
+	return ((char *)ht_get(server->ht, key));
+	return NULL;
+}
+
+void free_server_memory(server_memory* server) {
+	ht_free(server->ht);
+	free(server);
+}
